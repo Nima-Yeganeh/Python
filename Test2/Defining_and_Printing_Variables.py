506c51aa865d102bb30f71df0c9c70af0a59257e
root <yeganeh.nima.d@gmail.com>
2023-08-08 12:10:46 +0330

    done

---
+++ Test2/Defining_and_Printing_Variables.py
@@ -0,0 +1,5 @@
+mySuperMessage = "Hello and welcome!"
+print(mySuperMessage)
+
+mySuperMessage = "This is the best!"
+print(mySuperMessage)
---
+++ python3_install_on_ubuntu.sh
@@ -0,0 +1,5 @@
+apt install sudo -y
+sudo apt update -y
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
+python3 --version
